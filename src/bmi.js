c6f843092db5599494852afc75194c0dbf25f4cc
Nhooearn <99580726+Nhooearn@users.noreply.github.com>
2023-10-10 14:31:03 +0000

    add bmi.css

--- src/bmi.js
+++ src/bmi.js
@@ -1,15 +1,27 @@
 import { useRef, useState } from 'react';
-
+import './bmi.css'
 const BmiText = ({ bmi }) => {
-    if (bmi === 0 || bmi === null)
-        return (<h1>กรุณากรอกข้อมูล</h1>);
-    if (bmi < 18.5)
-        return (<h1>ผอม</h1>);
-    if (bmi > 30)
-        return (<h1>อ้วน</h1>);
+    let textColor;
 
-    return (<h1>ปรกติ</h1>);
+    if (bmi === 0 || bmi === null) {
+        textColor = 'red';
+        return (<h1 style={{ color: textColor }}>กรุณากรอกข้อมูล</h1>);
+    }
+
+    if (bmi < 18.5) {
+        textColor = 'blue';
+        return (<h1 style={{ color: textColor }}>ผอม</h1>);
+    }
+
+    if (bmi > 30) {
+        textColor = 'purple';
+        return (<h1 style={{ color: textColor }}>อ้วน</h1>);
+    }
+
+    textColor = 'green';
+    return (<h1 style={{ color: textColor }}>ปรกติ</h1>);
 }
+
 
 export default function Bmi() {
     const w_inputRef = useRef(null);
@@ -30,17 +42,26 @@ export default function Bmi() {
 
     return (
         <>
-            <h1>BMI Calculator</h1>
-            Weight : <input
-                type="text"
-                ref={w_inputRef} /> kg.<br />
-            Height : <input
-                type="text"
-                ref={h_inputRef} /> cm.<br />
-            <button onClick={calBmi}>Calculate!</button>
-            <button onClick={reset}>Reset</button><br />
-            Bmi value: {Bmi.toFixed(2)}
-            <BmiText bmi={Bmi} />
+            <div className='container'>
+                <h2>BMI Calculator</h2>
+
+                <div className='text-input'>
+                    Weight : <input
+                        type="text"
+                        ref={w_inputRef} /> kg.<br />
+                    Height : <input
+                        type="text"
+                        ref={h_inputRef} /> cm.<br />
+                </div>
+
+                <div className='bt'>
+                    <button onClick={calBmi}>Calculate!</button>
+                    <button onClick={reset}>Reset</button><br />
+                </div>
+
+                Bmi value : {Bmi.toFixed(2)}
+                <BmiText bmi={Bmi} />
+            </div>
         </>
     );
 }
